290b3a2178cb5261ef7c1348ce26f8fc4a38c612
ale <alex89.ficus@gmail.com>
2022-12-26 00:43:17 +0100

    Improved command parsing:
    * Moved from re.split to re.findall
    * introduced specific formats for base 16, 8 and 2
    * introduced identifiers
    * whitespaces always ignored

--- src/shrimp.py
+++ src/shrimp.py
@@ -7,18 +7,25 @@ Created on Sun Dec 25 23:10:13 2022
 
 import re
 
+TEST_STRING = r'2e3 pi 0xAf7 0777 0b1101 0B101 4 5+e*/ atan2 2,7 asdf -3.14E6.7'
+
 # Class definition
 class RPNcalc:
     def __init__(self):
         self.stack = []
         
     def load(self, data):
-        # Whitespace splitting guaranteed only if implemented outside!
-        toks = re.split(r'(-?\d+(?:\.\d+)?(?:[eE]-?\d)?)', data)
-        print(toks)
+        re_tok_str = (
+            r'0[xX][0-9a-fA-F]+|' # base 16, integer only
+            r'0[0-7]+|' # base 8, integer only
+            r'0[bB][01]+|' # base 2, integer only
+            r'-?\d+(?:\.\d+)?(?:[eE]-?\d)?|' # base 10, possibly exp notation
+            r'[\+\-\*/]|' # common operators
+            r'\w+' # remaining identifiers
+            )
+        toks = re.findall(re_tok_str, data)
         for tok in toks:
-            if tok is not None and tok != '':
-                self.stack.append(tok)
+            self.stack.append(tok)
         
     def get(self):
         return self.stack.pop()
