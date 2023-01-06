95f76c92c6c963a6dabfb784030d688c5e6bc5a5
ale <alex89.ficus@gmail.com>
2023-01-06 23:03:26 +0100

    Basically, 1st release candidate, working
    * Now the calculator inherits the list
    * Possibility to have functions to return tuples or nothing (to manage stack functions)
    * Dictionaries become defaults, but are mutable fields
    * Many functions added

--- src/rpncalc.py
+++ src/rpncalc.py
@@ -9,52 +9,76 @@ import re
 import numbers
 import math
 
-# Class definition
-class RPNcalc:
+# Class definition, inherits from standard lists
+class RPNcalc(list):
+    
+    PARSE_DICT_DEFAULT = {
+        'int_hex': (r'[\+\-]?0[xX][0-9a-fA-F]+', lambda x: int(x, 16)), # base 16, integer only
+        'int_oct': (r'[\+\-]?0[oO][0-7]+', lambda x: int(x, 8)), # base 8, integer only
+        'int_bin': (r'[\+\-]?0[bB][01]+', lambda x: int(x, 2)), # base 2, integer only
+        'number' : (r'[\+\-]?\d+(?:\.\d+)?(?:[eE][\+\-]?\d+)?', lambda x: float(x)), # base 10, possibly exp notation
+        'oper'   : (r'[\+\-\*/]', lambda x: str(x)), # common operators
+        'func'   : (r'\w+', lambda x: str(x)), # remaining functions
+        'error'  : (r'\S+', lambda x: None) # all the rest is error, unless whitespace
+        }
+    
+    FUNC_DICT_DEFAULT = {
+        # common operators
+        '+': (2, lambda x, y: y + x),
+        '-': (2, lambda x, y: y - x),
+        '*': (2, lambda x, y: y * x),
+        '/': (2, lambda x, y: y / x),
+        # stack manipulation
+        'swap': (2, lambda x, y: (x, y)),
+        'pop': (1, lambda x: None),
+        'dup': (1, lambda x: (x, x)),
+        # power
+        'pow': (2, lambda x, y: y**x),
+        'sqrt': (1, math.sqrt),
+        'root': (2, lambda x, y: y**(1/x)), # Python 3 only!
+        # trigonometric functions
+        'sin': (1, math.sin),
+        'cos': (1, math.cos),
+        'tan': (1, math.tan),
+        'asin': (1, math.asin),
+        'acos': (1, math.acos),
+        'atan': (1, math.atan),
+        'atan2': (2, math.atan2),
+        # transcendental functions
+        'exp': (1, math.exp),
+        'ln': (1, math.log),
+        'log': (1, math.log10),
+        # constants
+        'pi': (0, lambda: math.pi),
+        'e': (0, lambda: math.exp(1))
+        }
+    
     def __init__(self):
-        self.stack = []
+        list.__init__(self)
+        self.parse_dict = RPNcalc.PARSE_DICT_DEFAULT
+        self.func_dict = RPNcalc.FUNC_DICT_DEFAULT
+        self.all_regex = '|'.join('(?P<%s>%s)' % 
+                                  (key, val[0]) for key, val in self.parse_dict.items())
         
-    def load(self, data):
-        PARSE_DICT = {
-            'int_hex': (r'[\+\-]?0[xX][0-9a-fA-F]+', lambda x: int(x, 16)), # base 16, integer only
-            'int_oct': (r'[\+\-]?0[oO][0-7]+', lambda x: int(x, 8)), # base 8, integer only
-            'int_bin': (r'[\+\-]?0[bB][01]+', lambda x: int(x, 2)), # base 2, integer only
-            'number' : (r'[\+\-]?\d+(?:\.\d+)?(?:[eE][\+\-]?\d+)?', lambda x: float(x)), # base 10, possibly exp notation
-            'oper'   : (r'[\+\-\*/]', lambda x: str(x)), # common operators
-            'func'   : (r'\w+', lambda x: str(x)), # remaining functions
-            'error'  : (r'\S+', lambda x: None) # all the rest is error, unless whitespace
-            }
-        FUNC_DICT = {
-            '+': (2, lambda x, y: y + x),
-            '-': (2, lambda x, y: y - x),
-            '*': (2, lambda x, y: y * x),
-            '/': (2, lambda x, y: y / x),
-            'sin': (1, math.sin),
-            'pi': (0, math.pi)
-            }
-        all_regex = '|'.join('(?P<%s>%s)' % (key, val[0]) for key, val in PARSE_DICT.items())
-        for tok in re.finditer(all_regex, data):
+    def proc(self, data):
+        for tok in re.finditer(self.all_regex, data):
             value, tok_type = tok.group(0), tok.lastgroup
-            print('%10s: %10s' % (value, tok_type))
-            value = PARSE_DICT[tok_type][1](value)
-            if isinstance(value, numbers.Number):
-                self.stack.append(value)
-            elif tok_type == 'oper' or tok_type == 'func':
-                if value in FUNC_DICT:
-                    num_args, func = FUNC_DICT[value]
+            value = self.parse_dict[tok_type][1](value)
+            if isinstance(value, numbers.Number): # numbers are pushed on the stack
+                self.append(value)
+            elif tok_type == 'oper' or tok_type == 'func': # functions are processed
+                if value in self.func_dict:
+                    num_args, func = self.func_dict[value]
                     args = []
                     for cnt in range(num_args):
-                        args.append(self.stack.pop())
-                    if num_args > 0:
-                        self.stack.append(func(*args))
+                        args.append(self.pop())
+                    res = func(*args)
+                    if res == None:
+                        pass
+                    elif isinstance(res, tuple): # TODO improve with a unique statement
+                        self.extend(res)
                     else:
-                        self.stack.append(func) # not a real function, just a constant
-                    print(f'operands {num_args}:{args}')
+                        self.append(res)
             else:
                 print('Error!')
-        
-    def get(self):
-        return self.stack.pop()
-        
-    def get_all(self):
-        return self.stack
+     
--- src/shrimp.py
+++ src/shrimp.py
@@ -2,15 +2,19 @@ import sys
 import rpncalc
 
 TEST_REAL = '2 3*5+ 3/'
-TEST_TRIG = 'pi 2/ sin'
+TEST_TRIG = 'pi 2 / sin'
 TEST_STRING = '0x47 2e3 1.3e17 pi \t -1.2e-1 0xAf7 0o777 0b1101 -0B101 4 5+e*/ atan2 2,7 asdf -3.14E+6.7'
 
 if __name__ == "__main__":
     
     print("SHRIMP - RPN calculator")
-    
     calc = rpncalc.RPNcalc()
-    for arg in sys.argv[1:]:
-        calc.load(arg)
     
-    print(calc.get_all())
+    for arg in sys.argv[1:]:
+        calc.proc(arg)
+        print(calc)
+    
+    for line in sys.stdin:
+        calc.proc(line)
+        print(calc)
+        
